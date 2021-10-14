53c79901788696774af3b1314014d81f224a76c4
CarlosGenis <carlosgenis06@gmail.com>
2021-10-14 19:19:03 -0400

    Update script.js
    added button listener and wrote the first function

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ let numRows = 0;
 let numCols = 0;
 let colorSelected;
 
+var buttons = document.getElementsByTagName("button")
+    addRow = button[0],
+    addCol = button[1],
+    removeRow = button[2],
+    removeCol = button[3],
+    fillAll = button[4],
+    fillColor = button[5],
+    clearAll = button[6];
+//when clicked it calls the add row function
+addRow.addEventListener("click", addR);
+
+
+let grid = document.getElementById("grid");//call the grid id in index.html
+let tr = document.createElement("tr");
+grid.appendChild(tr);
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
-    let grid = document.getElementById("grid");//call the grid id in index.html
-    let tr = document.createElement("tr");
-    grid.appendChild(tr);
+     alert("Clicked Add Row")
+     numRows += 1
+     console.log(tr);
 }
 //Add a column
 function addC() {
